240892cbc92321316cf06769775b85274dd7efcd
MOON <18384217426@163.com>
2017-05-15 15:20:20 +0800

    20170427：开始加入react-native实现软件的部分界面。 目前仅仅完成react-native模块的嵌入。

--- app/src/main/java/com/kiplening/demo/common/MyApplication.java
+++ app/src/main/java/com/kiplening/demo/common/MyApplication.java
@@ -2,15 +2,23 @@ package com.kiplening.demo.common;
 
 import android.app.Application;
 
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.kiplening.demo.BuildConfig;
 import com.kiplening.demo.module.Settings;
+import com.kiplening.demo.reactNativeTrans.TransMissionPackage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by MOON on 3/2/2016.
  */
-public class MyApplication extends Application {
+public class MyApplication extends Application implements ReactApplication{
 
     public static MyApplication instance;
     public static MyApplication getInstance(){return instance;}
@@ -31,5 +39,36 @@ public class MyApplication extends Application {
         this.lockList = new ArrayList<>();
         this.booleanStates = new HashMap<>();
         //SharedPreferences mSharedPreferences = this.getSharedPreferences("settings",MODE_PRIVATE);
+    }
+
+    private static final TransMissionPackage mTransMissionPackage = new TransMissionPackage();
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    mTransMissionPackage
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+    /**
+     * 获取 reactPackage
+     * @return
+     */
+    public static TransMissionPackage getReactPackage() {
+        return mTransMissionPackage;
     }
 }
